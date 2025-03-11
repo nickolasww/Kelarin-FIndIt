3d82b90722433deea1d88c43dbd93f5feee0d7b7
your_github_username <your_github_email>
2025-03-11 11:37:25 +0700

    feat: Navbar position (not completed)

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,19 @@
-
+import Link from "next/link"
 
 export default function Navbar () {
     return (
-        <section>
-            <div>
-                <h1>Navbar</h1>
+        <section >
+            <div className="flex justify-between items-center py-5 px-10  mx-auto ">
+                <h1>KELARIN </h1>
+                <ul className="flex gap-10 items-center"> 
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Pricing</li>
+                    <li>Contact</li>
+                <button className="bg-purple-800 p-2 rounded-md">
+                    Get Started
+                </button>
+                </ul> 
             </div>
         </section>
     )
