9ba39ad56d8f7f9db8af4c6411d0bb683bec44b3
Ww <nickolaswewe2@gmail.com>
2025-04-09 22:50:39 +0700

    feat: delete workspace modal

--- src/components/modal/deletemodal/index.tsx
+++ src/components/modal/deletemodal/index.tsx
@@ -3,6 +3,9 @@
 import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
+import Button from "@/components/button/index";
+import DeleteIcon from "@/assets/icon/DeleteIcon.svg";
+import Image from "next/image";
 
 interface DeleteModalProps {
     isOpen: boolean
@@ -19,9 +22,7 @@ const DeleteModal: React.FC<DeleteModalProps> = ({ isOpen, onClose, onCreate }) 
 
   return isModalOpen ? (
     <div className="fixed z-50 inset-0 bg-black/50 flex items-center justify-center p-4">
-    <div className="bg-white rounded-lg shadow-xl w-full max-w-md p-6 relative">
-      <div className="flex items-center justify-between ">
-      <h1> Ini modal delete </h1>
+    <div className="bg-white rounded-lg shadow-xl w-full max-w-2xl p-6 relative">
       <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -33,8 +34,26 @@ const DeleteModal: React.FC<DeleteModalProps> = ({ isOpen, onClose, onCreate }) 
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
+    <div className='flex flex-col gap-5 items-center justify-center'>
+      <h1 className='font-bold text-3xl text-purple-700'>Delete This Workspace?</h1>
+      <p className='px-40 text-center mb-5'>Are you sure want to delete this workspace? This action cannot be undone</p>
+      <Image
+      src={DeleteIcon} 
+      alt="Delete Icon"
+      />
+      <div className='flex gap-10'>
+      <Button
+        text="Delete"
+        className="border-purple-500 border-2 text-purple-500 font-semibold py-2 px-26 rounded mt-4"
+      />
+      <Button
+        text="Cancel"
+        className=" bg-purple-800 text-white font-semibold py-2 px-26 rounded mt-4 ml-2"
+      />
+      </div>
     </div>
-  </div>
+    </div>
+
   </div>
 ) : null;
 };
